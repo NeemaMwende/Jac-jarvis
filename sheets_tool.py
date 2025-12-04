8a06e0bd035d4ddcb12234b3c6945cb3a92dc00c
Neema Mwende <gracepete12@gmail.com>
2025-12-04 21:04:05 +0300

    contacts configuration updates

--- sheets_tool.py
+++ sheets_tool.py
@@ -1,24 +1,24 @@
 import csv
 import requests
 
-def get_contacts_public_csv(csv_url: str):
+def get_email_from_contacts(name: str) -> str:
+    csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTQgbznsuqeyHxSntiPhP9CQNnk35ny8JU-kL8pC_-8PSDp5xNF9K6FFlL2YP1e_Jv-NwBPdDrGUhz7/pub?gid=0&single=true&output=csv"
+
     response = requests.get(csv_url)
     response.raise_for_status()
 
-    decoded = response.content.decode("utf-8").splitlines()
+    decoded = response.content.decode("utf-8", errors="replace").splitlines()
     reader = csv.reader(decoded)
-
     next(reader)  # skip header
 
-    contacts = {}
+    name = name.strip().lower()
     for row in reader:
-        first_name = row[0].lower()
-        email = row[3]
-        contacts[first_name] = email 
+        first_name = row[0].strip().lower()
+        email = row[3].strip()
+        if first_name == name:
+            return email
 
-    return contacts
-
+    return f"No email found for {name}"
 
 if __name__ == "__main__":
-    csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTQgbznsuqeyHxSntiPhP9CQNnk35ny8JU-kL8pC_-8PSDp5xNF9K6FFlL2YP1e_Jv-NwBPdDrGUhz7/pub?gid=0&single=true&output=csv"
-    print(get_contacts_public_csv(csv_url))
+    print(get_email_from_contacts("catherine"))
