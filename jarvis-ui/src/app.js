fc9e3c85eeab067e4a9f3e0cf3221edd817f1717
Neema Mwende <gracepete12@gmail.com>
2025-12-05 01:02:49 +0300

    routing in jac

--- jarvis-ui/src/app.js
+++ jarvis-ui/src/app.js
@@ -1,25 +1,16 @@
 import {__jacJsx, __jacSpawn} from "@jac-client/utils";
-import { useState } from "react";
-function app() {
-  let [todos, setTodos] = useState([]);
-  let [input, setInput] = useState("");
-  async function addTodo() {
-    if (!input.trim()) {
-      return;
-    }
-    let response = await __jacSpawn("create_todo", "", {"text": input.trim()});
-    let new_todo = response.reports[0][0];
-    setTodos(todos.concat([new_todo]));
-    setInput("");
-  }
-  return __jacJsx("div", {}, [__jacJsx("h2", {}, ["My Todos"]), __jacJsx("input", {"value": input, "onChange": e => {
-    setInput(e.target.value);
-  }, "onKeyPress": e => {
-    if (e.key === "Enter") {
-      addTodo();
-    }
-  }}, []), __jacJsx("button", {"onClick": addTodo}, ["Add Todo"]), __jacJsx("div", {}, [todos.map(todo => {
-    return __jacJsx("div", {"key": todo._jac_id}, [__jacJsx("span", {}, [todo.text])]);
-  })])]);
+import { useState, useEffect } from "react";
+import { Router, Routes, Route, Link } from "@jac-client/utils";
+function Home() {
+  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["Home Page"]), __jacJsx("p", {}, ["Welcome to the home page!"])]);
 }
-export { app };
+function About() {
+  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["ℹ About Page"]), __jacJsx("p", {}, ["Learn more about our application."])]);
+}
+function Contact() {
+  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["Contact Page"]), __jacJsx("p", {}, ["Email: contact@example.com"])]);
+}
+function app() {
+  return __jacJsx(Router, {}, [__jacJsx("div", {}, [__jacJsx("nav", {}, [__jacJsx(Link, {"to": "/"}, ["Home"]), " | ", __jacJsx(Link, {"to": "/about"}, ["About"]), " | ", __jacJsx(Link, {"to": "/contact"}, ["Contact"])]), __jacJsx(Routes, {}, [__jacJsx(Route, {"path": "/", "element": __jacJsx(Home, {}, [])}, []), __jacJsx(Route, {"path": "/about", "element": __jacJsx(About, {}, [])}, []), __jacJsx(Route, {"path": "/contact", "element": __jacJsx(Contact, {}, [])}, [])])])]);
+}
+export { About, Contact, Home, app };
